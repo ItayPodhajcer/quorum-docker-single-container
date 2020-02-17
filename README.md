acd1d2d3f20f8c6c400858fe2a1dd61df4cf561c
Itay Podhajcer <itay@cladular.com>
2020-02-17 12:34:30 +0200

    Added initial scripts

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# quorum-docker-single-container
+# Header
+This repository contains the scripts disscused in the article [Header](URL).
+
+## Aditional Information
+Included wallet password: Xv4kx)2(Z$gg
---
+++ eng/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if ! test -f /qdata/dd/initialized; then
+    echo "Quorum initializing..."
+    geth --datadir=/qdata/dd init /qdata/dd/genesis.json
+
+    touch /qdata/dd/initialized
+else
+    echo "Quorum already initialized, skipping..."
+fi
+
+geth --datadir /qdata/dd --nodiscover --verbosity 6 --raft --raftport 50400 --rpc --rpcaddr 0.0.0.0 --rpcvhosts=* --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --emitcheckpoints --port 30303
