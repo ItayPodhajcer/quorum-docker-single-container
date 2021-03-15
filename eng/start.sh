a05036a67c5f34a8b890c145ad361f87990a0ff4
fox0430 <ipoduser0430@gmail.com>
2021-03-15 17:58:01 +0900

    Fix Quorum start script

--- eng/start.sh
+++ eng/start.sh
@@ -9,4 +9,7 @@ else
     echo "Quorum already initialized, skipping..."
 fi
 
+export PRIVATE_CONFIG=ignore
+export QUORUM_CONSENSUS=raft
+
 geth --datadir /qdata/dd --nodiscover --verbosity 6 --raft --raftport 50400 --rpc --rpcaddr 0.0.0.0 --rpcvhosts=* --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --emitcheckpoints --port 30303 --allow-insecure-unlock
